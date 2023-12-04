202c5dbda4fd0cc9d930be3cec6dbc968c41b8dc
sirhmvfx <sirhmvfx@gmail.com>
2023-12-05 00:22:05 +0100

    working on the homepage

--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -23,9 +23,26 @@ const Homepage = () => {
             </Link>
           </div>
 
-          <div>
-            <div>
-              <Image src="/images/image 4.png" alt="money" />
+          <div className="flex gap-5">
+            <div className="bg-green-100 w-2/4 rounded-lg p-6 flex flex-col items-center justify-center">
+              <Image
+                src="/images/image 4.png"
+                alt="money"
+                width={70}
+                height={70}
+              />
+
+              <h2 className="text-black">Money</h2>
+            </div>
+            <div className="bg-red-100 w-2/4 rounded-lg p-6 flex flex-col items-center justify-center">
+              <Image
+                src="/images/image 5.png"
+                alt="money"
+                width={70}
+                height={70}
+              />
+
+              <h2 className="text-black">Chocolate</h2>
             </div>
           </div>
         </div>
