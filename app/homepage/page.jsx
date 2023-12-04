1f14389f2c3103e99568fe7c6a5794569aac1865
sirhmvfx <sirhmvfx@gmail.com>
2023-12-05 00:15:20 +0100

    working on the homepage

---
+++ app/homepage/page.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import { PiHandHeartBold } from "react-icons/pi";
+
+const Homepage = () => {
+  return (
+    <>
+      <section className="  p-2 flex flex-col justify-center items-center bg-[#FFFDFA] h-screen">
+        <div className="py-4">
+          <h1 className="text-2xl font-semibold text-black">Promise Card</h1>
+          <h2 className="text-black">God Loves Cheerful Givers </h2>
+        </div>
+
+        <div className="w-full px-6 lg:w-2/4">
+          <div className="flex justify-between">
+            <div className="flex text-black">
+              <h2>Promise me</h2>
+              <PiHandHeartBold />
+            </div>
+
+            <Link href="/" className="text-black">
+              See All
+            </Link>
+          </div>
+
+          <div>
+            <div>
+              <Image src="/images/image 4.png" alt="money" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Homepage;
