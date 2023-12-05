06893aa3ff96f4bd39d66d7b55505d10bb4fd3cc
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-12-04 16:35:10 -0800

    added fonts

---
+++ font.js
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500"],
+});
