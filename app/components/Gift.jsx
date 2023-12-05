b9f132c9723aa53e1030f881d3af1a0e0e9d2cf2
sirhmvfx <sirhmvfx@gmail.com>
2023-12-05 09:59:29 +0100

    finished the home page

--- app/components/Gift.jsx
+++ app/components/Gift.jsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Gift = ({ id, title, img, bg }) => {
   return (
     <>
-      <div
+      <Link
+        href="/"
         key={id}
         className={`${bg} w-full rounded-lg p-6 flex flex-col items-center justify-center`}
       >
         <Image src={img} alt="money" width={70} height={70} />
 
         <h2 className=" text-black">{title}</h2>
-      </div>
+      </Link>
     </>
   );
 };
