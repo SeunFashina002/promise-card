a206b8de6d70839e17f97a79d4fcba193214fb41
sirhmvfx <sirhmvfx@gmail.com>
2023-12-05 09:09:45 +0100

    working on the homepage

---
+++ _data/data.jsx
@@ -0,0 +1,8 @@
+export const gifts = [
+  {
+    id: 1,
+    title: "Money",
+    img: <Image src="" alt="" width={70} height={70} />,
+    bg: "bg-",
+  },
+];
--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -7,18 +7,20 @@ const Homepage = () => {
     <>
       <section className="  p-2 flex flex-col justify-center items-center bg-[#FFFDFA] h-screen">
         <div className="py-4 text-center">
-          <h1 className="text-2xl font-semibold text-black">Promise Card</h1>
+          <h1 className="text-2xl font-semibold text-black font-heading">
+            Promise Card
+          </h1>
           <h2 className="text-black">God Loves Cheerful Givers </h2>
         </div>
 
         <div className="w-full px-6 lg:w-2/4">
           <div className="flex justify-between">
-            <div className="flex text-black">
+            <div className="flex items-center gap-2 text-black">
               <h2>Promise me</h2>
               <PiHandHeartBold />
             </div>
 
-            <Link href="/" className="text-black">
+            <Link href="/" className="text-primary">
               See All
             </Link>
           </div>
