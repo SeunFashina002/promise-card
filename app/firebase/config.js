9e13293b3f7811f9ca12c9faea9e4fd658ab69b0
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-12-16 22:35:57 -0800

    added storage

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {getStorage} from 'firebase/storage'
 import { getAnalytics } from "firebase/analytics";
 
 import {
@@ -22,10 +23,11 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app)
 
 setPersistence(auth, browserLocalPersistence)
 
 
 
-export { app, auth, db};
+export { app, auth, db, storage};
 
